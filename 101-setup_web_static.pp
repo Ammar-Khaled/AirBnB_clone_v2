fdd07c6d0fcb569b24ac8307412547e60dceb576
Ammar Khaled Abdel Nour Ibrahim Ali <111376558+Ammar-Khaled@users.noreply.github.com>
2023-09-11 07:34:20 +0300

    Update 101-setup_web_static.pp

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -2,5 +2,5 @@
 
 exec { 'deploy':
   provider => shell,
-  command  => "apt-get -y update ; apt-get -y install nginx ; service nginx start ; mkdir -p /data/web_static/shared/ ; mkdir -p /data/web_static/releases/test/ ; echo 'Alx School' > /data/web_static/releases/test/index.html ; ln -sf /data/web_static/releases/test/ /data/web_static/current ; chown -R ubuntu:ubuntu /data/ ; sed -i '41i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-available/default ; sudo service nginx restart"
+  command  => "sudo apt-get -y update ; sudo apt-get -y install nginx ; sudo service nginx start ; sudo mkdir -p /data/web_static/shared/ ; sudo mkdir -p /data/web_static/releases/test/ ; sudo echo 'Holberton School' > /data/web_static/releases/test/index.html ; sudo ln -sf /data/web_static/releases/test/ /data/web_static/current ; sudo chown -R ubuntu:ubuntu /data/ ; sudo sed -i '41i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-available/default ; sudo service nginx restart"
 }
